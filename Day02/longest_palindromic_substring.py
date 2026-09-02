9ce90abe2d11f8ccd314b4c6aa529131b15653d0
Suhani Mehra <mehrasuhani.sm88@gmail.com>
2026-09-02 13:18:51 +0530

    Complete assignment

--- Day02/longest_palindromic_substring.py
+++ Day02/longest_palindromic_substring.py
@@ -1,5 +1,15 @@
 def palindrome():
 
-    str1=input("Enter a string")
+    str1=input("Enter a string: ")
+    str2=""
 
+    for i in range(len(str1)):
+        for j in range(i+1 , len(str1)+1):
+            sub_string=str1[i:j]
+            if sub_string==sub_string[::-1]:
+                if len(sub_string)>len(str2):
+                    str2=sub_string
+    
+
+    print(str2)
 palindrome()
--- Day04/course_feedback.py
+++ Day04/course_feedback.py
@@ -1,36 +1,34 @@
-# feedback_data = {
-#     "Python Programming": [5, 4, "4", "Great", 5],
-#     "Machine Learning": [],
-#     "Deep Learning": ["Good", "Average", None]
-# }
-
-# ratings_dict={key:course;value:ratings}
-
-# def compile_feedback(ratings_dict):
-    
-#     pass
+feedback_data = {
+    "Python Programming": [5, 4, "4", "Great", 5],
+    "Machine Learning": [],
+    "Deep Learning": ["Good", "Average", None]
+}
 
 
-# def compile_feedback(ratings_dict):
+def compile_feedback(ratings_dict):
 
-#     result = {}
+    result = {}
 
-#     for course, ratings in ratings_dict.items():
+    for course, ratings in ratings_dict.items():
 
-#         valid_ratings = []
+        valid_ratings = []
 
-#         for rating in ratings:
-#             try:
-#                 valid_ratings.append(float(rating))
-#             except (ValueError, TypeError):
-#                 print(f"Warning: Invalid rating value '{rating}' in course '{course}' skipped.")
+        for rating in ratings:
+            try:
+                valid_ratings.append(float(rating))
+            except (ValueError, TypeError):
+                print(f"Warning: Invalid rating value '{rating}' in course '{course}' skipped.")
 
-#         try:
-#             average = sum(valid_ratings) / len(valid_ratings)
-#             result[course] = round(average, 2)
+        try:
+            average = sum(valid_ratings) / len(valid_ratings)
+            result[course] = round(average, 2)
 
-#         except ZeroDivisionError:
-#             print(f"Warning: No valid ratings found for course '{course}'. Rating set to 0.0.")
-#             result[course] = 0.0
+        except ZeroDivisionError:
+            print(f"Warning: No valid ratings found for course '{course}'. Rating set to 0.0.")
+            result[course] = 0.0
 
-#     return result
+    return result
+
+
+result=compile_feedback(feedback_data)
+print(result)
