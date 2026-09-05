47bad01a5ff373154380afb25bcaca21392d73e9
DBDA76 <DBDA76@ACTS.CDACECBLR.COM>
2026-09-05 16:48:15 +0530

    Student management haf done

--- practice_lab_assessment/Student_grade_assesment.py
+++ practice_lab_assessment/Student_grade_assesment.py
@@ -2,13 +2,14 @@ import json
 
 students = [
 {"id": 1, "name": "Aarav Sharma", "course": "Python Core", "marks": 88.5, "grade": "A"},
-{"id": 2, "name": "Diya Patel", "course": "Data Science", "marks": 74.0, "grade": "B"}
+{"id": 2, "name": "Diya Patel", "course": "Data Science", "marks": 74.0, "grade": "B"},
+{"id": 3, "name": "Diya Patel", "course": "Data Science", "marks": 74.0, "grade": "B"}
 ]
 
-id_counter=3
+id_counter=len(students)
 filename='students_record.json'
 def menu():
-    print('*'*50)
+    print('*'*100)
     menu_text=''' 
     Student Grade & Assessment Module
     [1] Enroll Student 
@@ -21,7 +22,7 @@ def menu():
     [8] Terminate
     '''
     print(menu_text)
-    print('*'*50)
+    print('*'*100)
 def grade_generator(marks):
     if marks>=85:
         grade='A'
@@ -55,11 +56,121 @@ def enroll_student():
             print("Marks cannot be less than 0 or more than 100.")
             return
         
-        students.append(dict(id=id_counter,name=name,course=course,marks=marks,grade=grade_generator(marks)))
+        students.append(dict(id=id_counter+1,name=name,course=course,marks=marks,grade=grade_generator(marks)))
         id_counter+=1
         print(students)
     except:
         print("Enter valid integer value.")
+
+def cohort_directory():
+
+    if len(students)==0:
+        print("No Records Found")
+    elif len(students)==1:
+        print_one_record(students[0])
+    else:
+        print_many_record(students)
+
+def print_one_record(s): #here s will represent students[0]
+
+    id,name,course,marks,grade=s.values()
+
+    print("------Student Details-------")
+
+    print(f'ID:        {id}')
+    print(f'Name:      {name}')
+    print(f'Course:    {course}')
+    print(f'Marks:     {marks:.2f}')
+    print(f'Grade:     {grade}')
+
+    print('-'*100)
+
+def print_many_record(students):
+
+    print("------Student Details-------")
+    print(f'{'ID':<5}{'Name':<20}{'Course':<20}{'Marks':>10}{'Grade':>10}')
+
+    print('-'*65)
+
+    for s in students:
+        id,name,course,marks,grade=s.values()
+
+        print(f'{id:<5}{name:<20}{course:<20}{marks:>10.2f}{grade:>10}')
+
+    print('-'*65)
+    
+def search_records():
+
+    try:
+        print(" Enter 1 to search by Student ID.\n Enter 2 to search by Student Name. \n Enter 3 to search by Course Name.")
+        search_choice=int(input())
+
+        if search_choice==1:
+    
+            search_by_student_id()
+
+        elif search_choice==2:
+            search_by_name()
+        elif search_choice==3:
+            pass
+        else:
+            print("Enter a Valid Input Choice.")
+    except:
+      print("Value Error")
+
+def search_by_student_id():
+    try:
+        student_id=int(input("Enter the Student ID you want to search."))
+        found=False
+
+        for student in students:
+            if student['id'] == student_id:
+                print_one_record(student)
+                print("Student Found")
+                found=True
+                break
+
+        if not found:
+            print(f"Student Not Found. ID: {student_id}")
+    except:
+        print("Enter a Valid Number")
+
+def search_by_name():
+    try:
+        student_name=input("Enter the Student ID you want to search.")
+        # res = [s for s in students if s["name"].strip().lower() == student_name.lower()]
+        res = []
+        for s in students:
+            if s["name"] == student_name:
+                res.append(dict(s))
+        
+        if len(res) == 0:
+            print(f"Student Not Found. Name: {student_name}")
+        elif len(res) == 1:
+            print_one_record(res[0])
+        else:
+            print_many_record(res)
+    except:
+        print("Enter a Valid Name")
+
+def search_by_course_name():
+    try:
+        course_name=input("Enter the Student ID you want to search.")
+        # res = [s for s in students if s["name"].strip().lower() == student_name.lower()]
+        res = []
+        for s in students:
+            if s["course"] == course_name:
+                res.append(dict(s))
+        
+        if len(res) == 0:
+            print(f"Student Not Found for Course Name: {course_name}")
+        elif len(res) == 1:
+            print_one_record(res[0])
+        else:
+            print_many_record(res)
+    except:
+        print("Enter a Valid Course Name")
+
 
 def save_to_json():
     global filename
@@ -74,10 +185,15 @@ def save_to_json():
 def load_from_json():
     global filename
     global students
+    global id_counter
     try:
         with open(filename,mode='r') as file:
             students=json.load(file)
+
+            id_counter=len(students)
+
         print("File Loaded successfully")
+
         
     except:
         print("File not loaded")
@@ -92,9 +208,9 @@ def main():
             case 1:
                 enroll_student()
             case 2:
-                pass
+                cohort_directory()
             case 3:
-                pass
+                search_records()
             case 4:
                 pass
             case 5:
