bc639ccd8597250b7a029351a63b94a6ef05c918
Suhanimehra <118792987+Suhanimehra@users.noreply.github.com>
2026-09-08 20:48:19 +0530

    employee complete

--- practice_lab_assessment/emloyee_anagement_system.py
+++ practice_lab_assessment/emloyee_anagement_system.py
@@ -27,7 +27,7 @@ def main():
                 case 4:
                     update_employees()
                 case 5:
-                    delete_eployees()
+                    delete_employees()
                 case 6:
                     calculate_payroll(employees)
                 case 7:
@@ -45,7 +45,7 @@ def main():
 
 def menu():
     print("="*60)
-    print("-----Employee Attendance & Payroll Managemnet System-----")
+    print("-----Employee Attendance & Payroll Management System-----")
     menu_text='''
     [1] Add Employee 
     [2] View Employees 
@@ -66,26 +66,30 @@ def add_employee():
     global id_counter
     try:
 
-        name=input("Enter Name of Employe: ")
+        name=input("Enter Name of Employee: ")
         if name.strip()=='':
             print("Name cannot be an empty string!")
+            return
 
         department=input("Enter Name of Department: ")
         if department.strip()=='':
             print("Name cannot be an empty string!")
+            return
 
         salary=float(input("Enter monthly salary: "))
         if not salary>0:
             print("Salary Cannot be negative!")
+            return
 
         days_present=int(input("Enter Days Present: "))
         if days_present not in range(0,27):
             print("Days cannot be less than 0 or more than 26!")
+            return
 
         status=calculate_status(days_present)
         print(status)
 
-        employees.append(dict(id=id_counter+1,name=name,department=department,salary=salary,days_present=days_present,satus=status))
+        employees.append(dict(id=id_counter+1,name=name,department=department,salary=salary,days_present=days_present,status=status))
         id_counter+=1
     except:
         print("Enter Valid Values!")
@@ -101,7 +105,7 @@ def calculate_status(days_present):
     if 15<=days_present<20:
         return "Warning"
 
-    if days_present<15:
+    else:
         return "Critical"
     
 
@@ -132,16 +136,18 @@ def print_many_record(employees):
 
     print("----Employee Details----")
 
-    print(f"{'ID':<5}{'Name':<10}{'Department':<15}{'Salary':>10}{'Days Present':>10}{'Status':>7}")
+    print(f"{'ID':<5}{'Name':<20}{'Department':<15}{'Salary':>10}{'Days Present':>20}{'Status':>10}")
     for e in employees:
         id,name,department,salary,days_present,status=e.values()
-        print(f"{id:<5}{name:<10}{department:<15}{salary:>10}{days_present:>10}{status:>7}")
+        
+        print(f"{id:<5}{name:<20}{department:<15}{salary:>10.2f}{days_present:>20}{status:>10}")
 
 
 def search_employees():
-
-    search_choice=int(input("Enter 1 for Search by ID.\n Enter 2 to search by Employee Name.\n Enter 3 to search by Department Name."))
-
+    try:
+        search_choice=int(input("Enter 1 for Search by ID.\n Enter 2 to search by Employee Name.\n Enter 3 to search by Department Name."))
+    except:
+        print("Enter Valid Value To Search!")
     if search_choice==1:
         search_by_id()
     elif search_choice==2:
@@ -159,6 +165,7 @@ def search_by_id():
 
         if not res:
             print(f"No Employee Found of {id_choice}")
+            return
 
         else:
             print_one_record(res[0])
@@ -168,10 +175,11 @@ def search_by_id():
 
 def search_by_name():
     try:
-        name_choice=int(input("Enter Employee Name to search: "))
+        name_choice=input("Enter Employee Name to search: ")
 
         if name_choice.strip()=='':
             print("Name cannot be an empty string!")
+            return 
 
 
         res =[e for e in employees if name_choice.lower().strip() in e['name'].lower()]
@@ -187,12 +195,13 @@ def search_by_name():
 
 def search_by_department():
     try:
-        dept_choice=int(input("Enter Department Name to search: "))
+        dept_choice=input("Enter Department Name to search: ")
 
         if dept_choice=='':
             print("Department Cannot be an empty string!")
+            return
 
-        res =[e for e in employees if dept_choice.lower().strip() in e['dept'].lower()]
+        res =[e for e in employees if dept_choice.lower().strip() in e['department'].lower()]
 
         if not res:
             print(f"No Employee Found of {dept_choice}")
@@ -204,84 +213,107 @@ def search_by_department():
         print("Input a Valid Name Value!")
 
 def update_employees():
-
-    update_choice=int(input("Enter The Employee ID to Update: "))
-    res =[e for e in employees if e['id']==update_choice]
-
-    if not res:
-        print("No Employee Found!")
-
-    else:
-        name=input("Enter Name of Employe: ")
-        if name.strip()=='':
-            print("Name cannot be an empty string!")
-
-        department=input("Enter Name of Department: ")
-        if department.strip()=='':
-            print("Name cannot be an empty string!")
-
-        salary=float(input("Enter monthly salary: "))
-        if not salary>0:
-            print("Salary Cannot be negative!")
-
-        days_present=int(input("Enter Days Present: "))
-        if days_present not in range(0,27):
-            print("Days cannot be less than 0 or more than 26!")
-
-        res[0]['name']=name
-        res[0]['department']=department
-        res[0]['salary']=salary
-        res[0]['days_present']=days_present
-        res[0]['status']=calculate_status(days_present)
-
-        print_many_record(employees)
-
-
-def delete_eployees():
-
-    delete_choice=int(input("Enter ID you want to Delete: "))
-
-    res=[e for e in employees if e['id']==delete_choice]
-
-    if not res:
-        print("No Employee Found !")
-
-    else:
-        sure_choice=("Are You Sure [y/n]: ")
-        if sure_choice.strip().lower()=='y':
-            employees.remove(res[0])
-            print("Employee Removed Successfully! ")
-            if len(employees)==0:
-                print("No Record Left!")
-            else:
-                print_many_record(employees)
-        else:
-            print("Nothing Deleted")
     
+    try:
+        update_choice=int(input("Enter The Employee ID to Update: "))
+        res =[e for e in employees if e['id']==update_choice]
+
+        if not res:
+            print("No Employee Found!")
+            return
+
+        else:
+            name=input("Enter Name of Employe: ")
+            if name.strip()=='':
+                print("Name cannot be an empty string!")
+                return
+
+            department=input("Enter Name of Department: ")
+            if department.strip()=='':
+                print("Name cannot be an empty string!")
+                return
+
+            salary=float(input("Enter monthly salary: "))
+            if not salary>0:
+                print("Salary Cannot be negative!")
+                return
+
+            days_present=int(input("Enter Days Present: "))
+            if days_present not in range(0,27):
+                print("Days cannot be less than 0 or more than 26!")
+                return
+
+            res[0]['name']=name
+            res[0]['department']=department
+            res[0]['salary']=salary
+            res[0]['days_present']=days_present
+            res[0]['status']=calculate_status(days_present)
+
+            print_many_record(employees)
+    except:
+        print("Try Again")
+
+def delete_employees():
+    try:
+        delete_choice=int(input("Enter ID you want to Delete: "))
+
+        res=[e for e in employees if e['id']==delete_choice]
+
+        if not res:
+            print("No Employee Found !")
+            return
+
+        else:
+            print_one_record(res[0])
+            sure_choice=input("Are You Sure [y/n]: ")
+            if sure_choice.strip().lower()=='y':
+                employees.remove(res[0])
+                print("Employee Removed Successfully! ")
+                if len(employees)==0:
+                    print("No Record Left!")
+                else:
+                    print_many_record(employees)
+            else:
+                print("Nothing Deleted")
+    except:
+        print("Valid Search Please")
+        
 def calculate_payroll(employees):
     for e in employees:
         salary=e['salary']
         present_days=e['days_present']
-        daily_salary=salary//26
+        daily_salary=salary/26
         absent_days=26-present_days
         deduction=daily_salary*absent_days
         final_salary=salary-deduction
 
-        print(final_salary)
+        
+        print(f"Payroll For Employess for {e['id']} is {final_salary}")
 
 
 def save_to_json():
     try:
         with open("employees.json", mode='w') as file:
 
-            json.dump(employees,file)
+            json.dump(employees,file,indent=4)
 
             print("File saved successfully")
 
     except:
         print("Not saved")
+        
 def load_to_json():
-    pass
+    
+    global employees,id_counter
+    try:
+        with open("employees.json", mode="r") as file:
+            employees=json.load(file)
+        
+            id_counter=len(employees)
+            
+        print("File Loaded Successfully")
+    except FileNotFoundError:
+        print("Not Loaded")
 
 if __name__=='__main__':
     main()
